29bd3e6e2c4c0889532d513c7c758cd0fbef5bbd
sjpark <sjpark@bilabhealthcare.com>
2022-01-26 16:19:14 +0900

    TextButton 적용(기존 FlatButton)

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Left Button Pressed!');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Right Button Pressed!');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

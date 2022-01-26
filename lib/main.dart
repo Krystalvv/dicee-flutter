53696e8c159ae3459626a3f2752ad013f006a40e
sjpark <sjpark@bilabhealthcare.com>
2022-01-26 16:41:24 +0900

    오른쪽 버튼도 동일하게 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -22,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -31,7 +33,7 @@ class _DicePageState extends State<DicePage> {
             child: TextButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 3;
+                  leftDiceNumber = Random().nextInt(6) + 1;
                   print('Dice Number : $leftDiceNumber');
                 });
               },
@@ -40,10 +42,13 @@ class _DicePageState extends State<DicePage> {
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
-                print('Right Button Pressed!');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  print('Dice Number : $rightDiceNumber');
+                });
               },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
